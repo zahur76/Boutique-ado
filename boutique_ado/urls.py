8cd27945851ed1aaf4c8bf5b7a7e710082e030c4
zahur76 <zahurmeerun@hotmail.com>
2020-12-02 12:35:46 +0000

    Add views and urls for products.html page

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     # Shows path were to obtain urls from instead of writing them here
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
