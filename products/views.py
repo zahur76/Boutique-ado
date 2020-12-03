de57f8700d5517d8ac6fc65230a014e21bdb7359
zahur76 <zahurmeerun@hotmail.com>
2020-12-03 07:01:30 +0000

    Add search for dropdown menus

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 # Import models database to use in search
-from .models import Product
+from .models import Product, Category
 
 
 # Create your views here.
@@ -10,11 +10,19 @@ def all_products(request):
     """ A view to return all products and product search """
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            # __name__ Looking for category field in product model
+            # (Products, category=categories)
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
-            # Query is blank
+            # Query is blank query= ""
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
@@ -25,6 +33,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
     return render(request, 'products/products.html', context)
 
