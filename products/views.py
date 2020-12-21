750f278813ec39ff95d25072b7895997a7bbd60a
zahur76 <zahurmeerun@hotmail.com>
2020-12-21 14:23:39 +0000

    Add security to edit and delete items

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+# requires login before editing or deleting item
+from django.contrib.auth.decorators import login_required
 # Import models database to use in search
 from .models import Product, Category
 from django.db.models.functions import Lower
@@ -91,8 +93,13 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         # This is a new product, no need to use instance
         form = ProductForm(request.POST, request.FILES)
@@ -114,8 +121,13 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+        
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         # This will get data from form and to update the product instance called above
@@ -140,8 +152,13 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
